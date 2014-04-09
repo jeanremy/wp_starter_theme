995dc4729efee45b4dac2b6942dd526bd7078812
JR <jeanremy@users.noreply.github.com>
2014-04-09 11:33:34 +0200

    change

--- page.php
+++ page.php
@@ -1,16 +1,16 @@
 <?php get_header(); ?>
 
 	<div id="content">
-		<div id="main" class="eightcol first clearfix" role="main">
+		<div id="main" class="" role="main">
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			<article id="post-<?php the_ID(); ?>" role="article" itemscope itemtype="http://schema.org/BlogPosting">
+			<article id="post-<?php the_ID(); ?>" role="article">
 				<header class="article_header">
-					<h1 class="page_title" itemprop="headline"><?php the_title(); ?></h1>
+					<h1 class="page_title"><?php the_title(); ?></h1>
 					<p class='post_metadata'><?php print 'Ecrit le ' . the_time( 'Y-m-j' ) . ' à ' the_time('H:i') . ' par ' . the_author() . ' dans ' . the_category(', ');
 					?></p>
 				</header> <!-- end article header -->
 
-				<section class="entry_content clearfix" itemprop="articleBody">
+				<section class="clearfix">
 					<?php the_content(); ?>
 				</section> <!-- end article section -->
 
@@ -21,7 +21,7 @@
 
 			<?php endwhile; else : ?>
 
-			<article id="post-not-found" class="hentry clearfix">
+			<article id="post-not-found" class="clearfix">
 				<header class="article-header">
 					<h1>Page non trouvée</P></h1>
 				</header>
