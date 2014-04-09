5e648e6680a93af251d945ed7b19bd5d9543e7bf
JR <jeanremy@users.noreply.github.com>
2014-04-09 11:35:51 +0200

    change

--- functions.php
+++ functions.php
@@ -14,12 +14,23 @@ define('TWITTER_ID', $options['tw_id']);
 # 		MENU
 #-----------------------------------
 add_theme_support( 'menus' );
+/*function register_nav() {
+  register_nav_menu('navigation',__( 'Navigation' ));
+}
+add_action( 'init', 'register_nav' );
+*/
 
 
 #-----------------------------------
 # 		MINIATURES
 #-----------------------------------
 add_theme_support( 'post-thumbnails' );
+/* Images sizes 
+
+add_image_size( 'tiers', 270);
+add_image_size( 'demi', 430 );
+add_image_size( 'full', 910 );*/
+
 
 
 #-----------------------------------
@@ -58,12 +69,14 @@ function theme_deregister_jquery() {
 add_action('wp_enqueue_scripts', 'theme_add_custom_scripts');
 function theme_add_custom_scripts() {
 	
-	wp_register_style('main_css', get_template_directory_uri() . '/css/main.min.css');
+	wp_register_style('main_css', get_template_directory_uri() . '/css/main.min.css');	
+	wp_register_script('modernizr', get_template_directory_uri() . '/js/vendor/modernizr.min.js', '', null, false);
 	wp_register_script('jquery', get_template_directory_uri() . '/js/vendor/jquery.min.js', '', '', true);
 	wp_register_script('bootstrap_js', get_template_directory_uri() . '/js/vendor/bootstrap.min.js', '', '', true);
 	wp_register_script('main_js', get_template_directory_uri() . '/js/main.min.js', array('jquery', 'bootstrap_js'), '', true );
 
 	wp_enqueue_style('main_css');
+	wp_enqueue_script('modernizr');	
 	wp_enqueue_script('main_js');
 }
 
