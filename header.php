af326f1bacd5bc5970a6f35be31edd2fc7da6b5e
JR <contact@jeanremypraud.com>
2014-06-10 17:14:48 +0200

    Change favicons

--- header.php
+++ header.php
@@ -10,24 +10,35 @@
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <!--Favicons & tiles-->
-        <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/img/favicon/apple-touch-icon.png">
-        <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/img/favicon/favicon.png">
+       <!--Favicons & tiles-->
+        <link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri(); ?>/apple-touch-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_template_directory_uri(); ?>/apple-touch-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/apple-touch-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri(); ?>/apple-touch-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/apple-touch-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_template_directory_uri(); ?>/apple-touch-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/apple-touch-icon-144x144.png">
+
+        <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/favicon-16x16.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="<?php echo get_template_directory_uri(); ?>/favicon-96x96.png">
+
+
         <!--[if IE]>
-            <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/img/favicon/favicon.ico">
+            <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
         <![endif]-->
-        <meta name="msapplication-TileColor" content="#290005">
-        <meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/img/favicon/win8-tile-icon.png">
+        <meta name="msapplication-TileColor" content="#f7f7f7">
+        <meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/win8-tile-icon-144x144.png">
 
         <!--Facebook-->
         <meta property="og:title" content="<?php bloginfo('name'); ?>" />
         <meta property="og:type" content="website">
         <meta property="og:url" content="<?php bloginfo('url'); ?>">
         <meta property="og:description" content="<?php bloginfo('description'); ?>" />
-        <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/img/favicon/facebook-icon.png" />
+        <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/facebook-icon.png" />
         <meta property="og:image:type" content="image/png">
-        <meta property="og:image:width" content="300">
-        <meta property="og:image:height" content="300">
+        <meta property="og:image:width" content="630">
+        <meta property="og:image:height" content="1200">
 
         <!--CSS & JS-->
         <?php wp_head(); ?>
