86e11cdcf6f72f7d9b6f6969e1263d0878a2434f
JR <jeanremy@users.noreply.github.com>
2014-12-16 17:22:29 +0100

    update favicon & deps to fit bower

--- header.php
+++ header.php
@@ -10,40 +10,38 @@
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <!--Favicons & tiles // http://realfavicongenerator.net/-->
         <!--Favicons & tiles-->
-        <link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri(); ?>/img/favicon/apple-touch-icon-57x57.png">
-        <link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_template_directory_uri(); ?>/img/favicon/apple-touch-icon-60x60.png">
-        <link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/img/favicon/apple-touch-icon-72x72.png">
-        <link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri(); ?>/img/favicon/apple-touch-icon-76x76.png">
-        <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/img/favicon/apple-touch-icon-114x114.png">
-        <link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_template_directory_uri(); ?>/img/favicon/apple-touch-icon-120x120.png">
-        <link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/img/favicon/apple-touch-icon-144x144.png">
-        <link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_template_directory_uri(); ?>/img/favicon/apple-touch-icon-152x152.png">
-        <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/img/favicon/favicon-16x16.png" sizes="16x16">
-        <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/img/favicon/favicon-32x32.png" sizes="32x32">
-        <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/img/favicon/favicon-96x96.png" sizes="96x96">
-        <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/img/favicon/favicon-160x160.png" sizes="160x160">
-        <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/img/favicon/favicon-196x196.png" sizes="196x196">
+        <link rel="apple-touch-icon" sizes="57x57" href="/img/favicon/apple-touch-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="/img/favicon/apple-touch-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="/img/favicon/apple-touch-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="/img/favicon/apple-touch-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="/img/favicon/apple-touch-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="/img/favicon/apple-touch-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="/img/favicon/apple-touch-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="/img/favicon/apple-touch-icon-152x152.png">
+        <link rel="icon" type="image/png" href="/img/favicon/favicon-16x16.png" sizes="16x16">
+        <link rel="icon" type="image/png" href="/img/favicon/favicon-32x32.png" sizes="32x32">
+        <link rel="icon" type="image/png" href="/img/favicon/favicon-96x96.png" sizes="96x96">
+        <link rel="icon" type="image/png" href="/img/favicon/favicon-160x160.png" sizes="160x160">
+        <link rel="icon" type="image/png" href="/img/favicon/favicon-196x196.png" sizes="196x196">
         <meta name="msapplication-TileColor" content="#fafafa">
-        <meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/img/favicon/win8-tile-icon-144x144.png">
+        <meta name="msapplication-TileImage" content="/img/favicon/win8-tile-icon-144x144.png">
 
-       
         <!--Facebook-->
-        <meta property="og:title" content="<?php bloginfo('name'); ?>" />
+        <meta property="og:title" content="" />
         <meta property="og:type" content="website">
-        <meta property="og:url" content="<?php bloginfo('url'); ?>">
-        <meta property="og:description" content="<?php bloginfo('description'); ?>" />
-        <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/facebook-icon.png" />
+        <meta property="og:url" content="">
+        <meta property="og:description" content="" />
+        <meta property="og:image" content="/img/favicon/facebook-icon.png" />
         <meta property="og:image:type" content="image/png">
-        <meta property="og:image:width" content="630">
-        <meta property="og:image:height" content="1200">
+        <meta property="og:image:width" content="600">
+        <meta property="og:image:height" content="400">
 
         <!--CSS & JS-->
         <?php wp_head(); ?>
          <!--[if lt IE 9]>
-            <script src="js/vendor/respond.min.js"></script>
-            <script src="js/vendor/html5shiv.min.js"></script>
+            <script src="js/vendor/respond/src/respond.js"></script>
+            <script src="js/vendor/html5shiv/dist/html5shiv.min.js"></script>
         <![endif]-->
     </head>
 
